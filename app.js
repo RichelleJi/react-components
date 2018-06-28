a5e31efc5b20607f319aa77bcd2460cad1fed1ff
richelleji <ruixi.ji@gmail.com>
2018-06-27 22:15:41 -0700

    refine

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 /*
-
+//React.createElement(component, props, ...children) 
 everything gets compiled to:
 React.createElement(component/props, {propsitems:[]}, ...children component)
 
@@ -88,36 +88,168 @@ ReactDOM.render(componentInstance, DOMElement)
 // };
 // */
 
-var TodoList = (props) => {
+// Prop: 
 
-  // This function will be called when the first `<li>` below is clicked on
-  // Notice that event handling functions receive an `event` object
-  // We want to define it where it has access to `props`
+// var TodoList = (props) => {
 
-  var onListItemClick = (event) => {
-    console.log('I got clicked');
-  };
+//   // This function will be called when the first `<li>` below is clicked on
+//   // Notice that event handling functions receive an `event` object
+//   // We want to define it where it has access to `props`
 
-  // Because we used curly braces with this arrow function
-  // we have to write an explicit `return` statement
-    //very similar to handling events on the DOM
-  return (
-    <ul>
-      <li onClick={onListItemClick}>
-      {props.todos[0]}</li>
-            {/* React events are named using camelCase, rather than lowercase.
-              With JSX you pass a function as the event handler, rather than a string.*/}
-      <li>{props.todos[1]}</li>
-      <li>{props.todos[2]}</li>
-    </ul>
-  );
+//   var onListItemClick = (event) => {
+//     console.log('I got clicked');
+//   };
+
+//   // Because we used curly braces with this arrow function
+//   // we have to write an explicit `return` statement
+//     //very similar to handling events on the DOM
+//   return (
+//     <ul>
+//       <li onClick={onListItemClick}>
+//       {props.todos[0]}</li>
+//             {/* React events are named using camelCase, rather than lowercase.
+//               With JSX you pass a function as the event handler, rather than a string.*/}
+//       <li>{props.todos[1]}</li>
+//       <li>{props.todos[2]}</li>
+//     </ul>
+//   );
+// }
+
+
+
+
+// // A class component can be defined as an ES6 class
+// // that extends the base Component class included in the React library
+
+// class TodoListItem extends React.Component {
+
+//   // A `constructor` method is expected on all ES6 classes
+//   // When React instantiates the component,
+//   // it will pass `props` to the constructor
+//   constructor(props) {
+//     // Equivalent to ES5's React.Component.call(this, props)
+//     super(props);
+//   }
+
+//   // Every class component must have a `render` method
+//   // Stateless functional components are pretty much just this method
+//   render() {
+
+//     // `props` is no longer passed as an argument,
+//     // but instead accessed with `this.props`
+//     console.log(this.props.todokey);
+//     return (
+//       <li>{this.props.todokey}</li>
+//     );
+
+//   }
+
+// }
+
+// // Update our `TodoList` to use the new `TodoListItem` component
+// // This can still be a stateless function component!
+// var TodoList = (props) => (
+//   <ul>
+//     {props.todos.map(eachtodo =>
+//       <TodoListItem todokey ={eachtodo} />
+//     )}
+//   </ul>
+// );
+
+// //    props.todos.map(function (eachtodo) {
+//     //   return React.createElement(TodoListItem, { todo: eachtodo });
+//     // })
+
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> 
+//   </div>
+// );
+
+// //    React.createElement(TodoList, { todos: ['Learn React', 'Crush Recast.ly', 'Maybe sleep'] })
+
+
+// ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
+
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // `state` is just an object literal
+    this.state = {
+      done: false
+    };
+  }
+
+  // When a list item is clicked, we will toggle the `done`
+  // boolean, and our component's `render` method will run again
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    // Making the style conditional on our `state` lets us 
+    // update it based on user interactions.
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    // You can pass inline styles using React's `style` attribute to any component
+    // snake-cased css properties become camelCased this this object
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+    );
+  }
 }
 
-var App = () => (
-  <div>
-    <h2>My Todo List</h2>
-    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
-  </div>
+
+// // Update our `TodoList` to use the new `TodoListItem` component
+// // This can still be a stateless function component!
+var TodoList = (props) => (
+  <ul>
+    {props.todos.map(todo =>
+      <TodoListItem todo={todo} />
+    )}
+  </ul>
 );
 
-ReactDOM.render(<App />, document.getElementById("app"));
+
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> 
+//   </div>
+// );
+
+
+
+// ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
+
+// class GroceryListItems extends React.component {
+//   constructor(props){
+//     super(props);
+//   }
+  
+//   rendere(){
+//     return(
+    
+    
+    
+//     );
+    
+//   }
+  
+// } 
+
+
+
+
